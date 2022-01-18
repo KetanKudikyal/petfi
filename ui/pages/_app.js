bcb83058613f61d6dfdc27e61a57387fbaa39622
vivekascoder <vivekascoder@gmail.com>
2022-01-19 02:14:03 +0530

    Added basic authentication using moralis

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { MoralisProvider } from "react-moralis";
+
+const appId = process.env.NEXT_PUBLIC_APP_ID;
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
